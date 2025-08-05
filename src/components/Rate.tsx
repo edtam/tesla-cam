ac141fc28785a2d92121866ec4fbe23159f43b3d
edtam <414689070@qq.com>
2025-08-05 10:10:33 +0800

    修改播放速度交互

--- src/components/Rate.tsx
+++ src/components/Rate.tsx
@@ -4,20 +4,19 @@ type Props = {
 };
 
 export function Rate({ value, onChange }: Props) {
-  const rates = [0.5, 1, 2, 3];
-
-  const changeRate = () => {
-    const index = rates.indexOf(value);
-    const rate = rates.at(index + 1);
-    onChange(rate || rates[0]);
-  };
+  const rates = [0.1, 0.5, 1, 2, 3, 4, 5, 6, 7, 8];
 
   return (
-    <button
-      className="cursor-pointer rounded-lg bg-neutral-800 px-4 py-2 hover:bg-neutral-700"
-      onClick={changeRate}
+    <select
+      className="cursor-pointer appearance-none rounded-lg bg-neutral-800 px-4 py-2 text-center hover:bg-neutral-700"
+      value={value}
+      onChange={(e) => onChange(Number(e.target.value))}
     >
-      {value}x
-    </button>
+      {rates.map((i) => (
+        <option key={i} value={i}>
+          {i}x
+        </option>
+      ))}
+    </select>
   );
 }
